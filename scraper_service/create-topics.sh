f04cc0dcdae69fc75ae6fd336724a6d969a7963a
GOrrego <133718375+GOrrego@users.noreply.github.com>
2024-12-23 00:01:18 -0300

    Delete scraper_service/create-topics.sh

--- scraper_service/create-topics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Crear tópico 'alerts'
-kafka-topics --create \
-  --topic alerts \
-  --bootstrap-server kafka:9092 \
-  --partitions 1 \
-  --replication-factor 1 || echo "El tópico ya existe."
